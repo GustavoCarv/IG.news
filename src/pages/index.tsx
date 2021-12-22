2350f78d01fd15a6bc1c8bc43963284051257d45
GustavoCarv <gustavo.carvg@gmail.com>
2021-12-22 16:37:00 -0300

    Startado o projeto em Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | IG.MainNews</title>
+      </Head>
+      <h1 >Hello Next.js</h1>
+    </>
+  );
+}
