357fbbb55f39773b99ae4fd69cbe46f575ad316e
GustavoCarv <gustavo.carvg@gmail.com>
2021-12-26 13:48:24 -0300

    iniciada a integração com o faunadb na aplicação

--- src/components/signinButton/index.tsx
+++ src/components/signinButton/index.tsx
@@ -1,17 +1,20 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn } from 'next-auth/react';
+import { signIn, useSession, signOut } from 'next-auth/react';
+
 
 
 export default function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession()
+ 
 
-  return isUserLoggedIn ? (
+
+  return (status === "authenticated") ? (
     <>
-      <button type="button" className={styles.signInButton}>
+      <button type="button" className={styles.signInButton} onClick={() => signOut()}>
         <FaGithub color="#04d361" />
-        Your Name Here :)
+        {session.user.name}
         <FiX color='737380' className={styles.closeIcon} />
       </button>
     </>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../../styles/global.scss";
 import { AppProps } from "next/app";
 import Header from "../components/header";
+import {SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
